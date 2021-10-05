3616fc1226fd6803ee2e19013dc19bbec00fda03
Diogo <diogofilipebotelho@gmail.com>
2021-10-05 19:47:56 -0400

    Code review

--- routes/shipments.js
+++ routes/shipments.js
@@ -19,14 +19,17 @@ const router = new express.Router();
 
 router.post("/", async function (req, res, next) {
   const { productId, name, addr, zip } = req.body;
+  // console.log(req.body);
 
-  const result = jsonschema.validate({
-    productId,
-    name,
-    addr,
-    zip
-  }, shipSchema);
+  // const result = jsonschema.validate({
+  //   productId,
+  //   name,
+  //   addr,
+  //   zip
+  // }, shipSchema);
   
+  const result = jsonschema.validate(req.body, shipSchema);
+
   if (!result.valid) {
     let errs = result.errors.map(err => err.stack);
     throw new BadRequestError(errs);
--- routes/shipments.test.js
+++ routes/shipments.test.js
@@ -16,6 +16,9 @@ describe("POST /", function () {
     expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
 
+    // Code review: Check what's in resp and test for the error messages,
+    //so we can tell it's failing for the reasons we presume it's failing for;
+
   test("Invalid JSON - missing field", async function () {
     const resp = await request(app).post("/shipments").send({
       productId: 1000,
@@ -50,4 +53,19 @@ describe("POST /", function () {
     expect(resp.statusCode).toEqual(400);
     // expect(resp.body).toEqual({ shipped: expect.any(Number) });
   });
+
+  test("Invalid JSON - additional parameter", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "New York",
+      zip: "12345-6789",
+      phone: "555555555"
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    // expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  //Code review: Add an additional property that's not expected;
 });
