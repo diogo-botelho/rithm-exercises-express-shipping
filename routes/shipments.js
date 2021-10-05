b577a77960563aac22ded1601ab69719dcf3a235
Diogo <diogofilipebotelho@gmail.com>
2021-10-05 19:29:11 -0400

    Added schema and json validation in shipments route

---
+++ routes/shipments.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const express = require("express");
+const jsonschema = require("jsonschema");
+
+const { shipProduct } = require("../shipItApi");
+const shipSchema = require("../schemas/shipSchema.json");
+const { BadRequestError } = require("../expressError");
+
+const router = new express.Router();
+
+/** POST /ship
+ *
+ * VShips an order coming from json body:
+ *   { productId, name, addr, zip }
+ *
+ * Returns { shipped: shipId }
+ */
+
+router.post("/", async function (req, res, next) {
+  const { productId, name, addr, zip } = req.body;
+
+  const result = jsonschema.validate({
+    productId,
+    name,
+    addr,
+    zip
+  }, shipSchema);
+  
+  if (!result.valid) {
+    let errs = result.errors.map(err => err.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const shipId = await shipProduct({ productId, name, addr, zip });
+  return res.json({ shipped: shipId });
+});
+
+module.exports = router;
---
+++ routes/shipments.test.js
@@ -0,0 +1,53 @@
+"use strict";
+
+const request = require("supertest");
+const app = require("../app");
+
+
+describe("POST /", function () {
+  test("valid", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("Invalid JSON - missing field", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "100 Test St",
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    // expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("Invalid JSON - invalid input type", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: 123,
+      addr: "100 Test St",
+      zip: "12345-6789",
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    // expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+
+  test("Invalid JSON - empty string input", async function () {
+    const resp = await request(app).post("/shipments").send({
+      productId: 1000,
+      name: "Test Tester",
+      addr: "",
+      zip: "12345-6789",
+    });
+
+    expect(resp.statusCode).toEqual(400);
+    // expect(resp.body).toEqual({ shipped: expect.any(Number) });
+  });
+});
